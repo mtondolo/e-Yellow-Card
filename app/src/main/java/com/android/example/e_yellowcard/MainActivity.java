ac1a1790fd0453d4e67caed60b778bcb21712269
mtondolo <mtondolo@gmail.com>
2018-09-24 17:21:30 +0200

    Create first layout

--- app/src/main/java/com/android/example/e_yellowcard/MainActivity.java
+++ app/src/main/java/com/android/example/e_yellowcard/MainActivity.java
@@ -2,12 +2,37 @@ package com.android.example.e_yellowcard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mYCListTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+         * Using findViewById, we get a reference to our TextView from xml. This allows us to
+         * do things like set the text of the TextView.
+         */
+        mYCListTextView = (TextView) findViewById(R.id.tv_yc);
+
+        /*
+         * This String array contains yellow card numbers.
+         *
+         */
+        String[] ycNumber = YellowCard.getYellowCardNumber();
+
+        /*
+         * Iterate through the array and append the Strings to the TextView. The purpose of
+         * the "\n\n\n" after the String is to give visual separation between each String in the
+         * TextView.
+         */
+        for (String yellowCardNumber : ycNumber) {
+            mYCListTextView.append(yellowCardNumber + "\n\n\n");
+        }
     }
 }
+
---
+++ app/src/main/java/com/android/example/e_yellowcard/YellowCard.java
@@ -0,0 +1,26 @@
+package com.android.example.e_yellowcard;
+
+public class YellowCard {
+
+    /**
+     * This method returns a list of new headlines.
+     *
+     * @return A list of news head lines
+     */
+    public static String[] getYellowCardNumber() {
+        return new String[]{
+                "TZ12345678",
+                "BR12345678",
+                "KE12345678",
+                "UG12345678",
+                "RW12345678",
+                "ZM12345678",
+                "ZW12345678",
+                "ML12345678",
+                "ET12345678",
+                "DJ12345678",
+                "SD12345678",
+                "ER12345678",
+        };
+    }
+}
