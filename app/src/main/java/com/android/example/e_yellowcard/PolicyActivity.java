7e1c86447fda0848f2f098be59b7ff92fb304111
mtondolo <mtondolo@gmail.com>
2018-09-26 15:05:03 +0200

    Load data with recyclerview

--- app/src/main/java/com/android/example/e_yellowcard/PolicyActivity.java
+++ app/src/main/java/com/android/example/e_yellowcard/PolicyActivity.java
@@ -3,6 +3,8 @@ package com.android.example.e_yellowcard;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -12,22 +14,23 @@ import com.android.example.e_yellowcard.utils.NetworkUtils;
 
 import java.net.URL;
 
-public class MainActivity extends AppCompatActivity {
+public class PolicyActivity extends AppCompatActivity {
 
-    private TextView mYCListTextView;
+    private RecyclerView mRecyclerView;
+    private PolicyAdapter mPolicyAdapter;
     private TextView mErrorMessageDisplay;
     private ProgressBar mLoadingIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_policy);
 
         /*
-         * Using findViewById, we get a reference to our TextView from xml. This allows us to
-         * do things like set the text of the TextView.
+         * Using findViewById, we get a reference to our RecyclerView from xml. This allows us to
+         * do things like set the adapter of the RecyclerView and toggle the visibility.
          */
-        mYCListTextView = (TextView) findViewById(R.id.tv_yc);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_policy);
 
         /* This TextView is used to display errors and will be hidden if there are no errors */
         mErrorMessageDisplay = (TextView) findViewById(R.id.tv_error_message_display);
@@ -38,6 +41,33 @@ public class MainActivity extends AppCompatActivity {
          *
          */
         mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+
+        /*
+         * LinearLayoutManager can support HORIZONTAL or VERTICAL orientations. The reverse layout
+         * parameter is useful mostly for HORIZONTAL layouts that should reverse for right to left
+         * languages.
+         */
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        /*
+         * Use setHasFixedSize(true) on mRecyclerView to designate that all items in the list
+         * will have the same size.
+         */
+        mRecyclerView.setHasFixedSize(true);
+
+        /*
+         * The NewsAdapter is responsible for linking our news data with the Views that will end up
+         * displaying our news data.
+         */
+        mPolicyAdapter = new PolicyAdapter();
+
+        /*
+         * Use mRecyclerView.setAdapter and pass in mNewsAdapter.
+         * Setting the adapter attaches it to the RecyclerView in our layout.
+         */
+        mRecyclerView.setAdapter(mPolicyAdapter);
 
         /* Once all of our views are setup, we can load the weather data. */
         loadYCPolicyData();
@@ -52,23 +82,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method will make the View for the news data visible and
-     * hide the error message.
+     * This method will make the View for the news data visible and hide the error message.
      */
     private void showYCPolicyDataView() {
         /* First, make sure the error is invisible */
         mErrorMessageDisplay.setVisibility(View.INVISIBLE);
         /* Then, make sure the policy data is visible */
-        mYCListTextView.setVisibility(View.VISIBLE);
+        mRecyclerView.setVisibility(View.VISIBLE);
     }
 
     /**
-     * This method will make the error message visible and hide the news
-     * View.
+     * This method will make the error message visible and hide the news View.
      */
     private void showErrorMessage() {
         /* First, hide the currently visible data */
-        mYCListTextView.setVisibility(View.INVISIBLE);
+        mRecyclerView.setVisibility(View.INVISIBLE);
         /* Then, show the error */
         mErrorMessageDisplay.setVisibility(View.VISIBLE);
     }
@@ -84,16 +112,13 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String[] doInBackground(String... params) {
-
             URL ycPolicyRequestUrl = NetworkUtils.buildYCPoliciesUrl();
-
             try {
-
                 String jsonYCPolicyResponse = NetworkUtils
                         .getResponseFromHttpUrl(ycPolicyRequestUrl);
 
                 String[] simpleJsonYCPolicyData = JSONUtils
-                        .getSimpleYCPolicyStringFromJson(MainActivity.this, jsonYCPolicyResponse);
+                        .getSimpleYCPolicyStringFromJson(PolicyActivity.this, jsonYCPolicyResponse);
                 return simpleJsonYCPolicyData;
 
             } catch (Exception e) {
@@ -104,26 +129,15 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String[] ycPolicyData) {
-            //As soon as the data is finished loading, hide the loading indicator
             mLoadingIndicator.setVisibility(View.INVISIBLE);
             if (ycPolicyData != null) {
-
-                // If the policy data was not null, make sure the data view is visible
                 showYCPolicyDataView();
-
-                /*
-                 * Iterate through the array and append the Strings to the TextView. We add
-                 * the "\n\n\n" after the String to give visual separation between each String in the
-                 * TextView.
-                 */
-                for (String ycPolicyString : ycPolicyData) {
-                    mYCListTextView.append((ycPolicyString) + "\n\n\n");
-                }
+                mPolicyAdapter.setPolicyData(ycPolicyData);
             } else {
-                // If the policy data was null, show the error message
                 showErrorMessage();
             }
         }
     }
 }
+
 
